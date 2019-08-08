0f7ed05455900cb9429b1be18d12fae9983ea46f
dh-kim7 <dhkim715@hotmail.com>
2019-08-08 17:56:18 +0900

    practice express code

---
+++ express/express_listen.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+app.listen(80);
+
+app.get('/', function(req, res){
+  res.send('Hello from Express');
+});
+
+// http://localhost/
---
+++ express/express_send_download.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var url = require('url');
+var app = express();
+app.listen(80);
+app.get('/download', function (req, res) {
+  res.sendfile('./views/word.docx', 'new.docx');
+});
+
+// http://localhost/download
